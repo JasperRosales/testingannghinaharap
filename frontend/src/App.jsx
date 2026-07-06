7985eb6c7efaa36aa46caf554c0abdfed9d495b4
kingkongdelavega-cmd <kingkongdelavega@gmail.com>
2026-07-07 05:58:39 +0800

    added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import TransactionSynchronizationInterface from "./components/week1/Day1/TransactionSynchronizationInterface";
+import InventoryMonitoringInterface from "./components/week2/Day1/InventoryMonitoringInterface";
+import CrossDeviceReportsInterface from "./components/week2/Day2/CrossDeviceReportsInterface";
+
+
+function App() {
+  return (
+    <>
+      <CrossDeviceReportsInterface/>
+    </>
+  );
+}
+//<TransactionSynchronizationInterface />
+//<InventoryMonitoringInterface/>
+
+export default App;
